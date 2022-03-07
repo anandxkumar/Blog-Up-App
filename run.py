e928cc2579ef4157c1ae8b46da4f3599066803df
anandxkumar <anandkumar26sep00@gmail.com>
2022-03-07 23:19:59 +0530

    Updated gitignore, added some details

--- run.py
+++ run.py
@@ -2,6 +2,6 @@ from flaskblog import app
 
 
 if __name__=="__main__": #if code runs from this file
-    app.run(host='127.0.0.9',port=4455,debug=False) 
+    app.run(host='0.0.0.0',port=4455,debug=False) 
     
     
